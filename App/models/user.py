3ecf35480c3d3a8a267ea5e4ff47e806c62c47aa
Augustine-dev <157492187+Augustine-dev@users.noreply.github.com>
2025-09-29 18:41:27 -0400

    Model creation

--- App/models/user.py
+++ App/models/user.py
@@ -4,16 +4,26 @@ from App.database import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(70), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
+    user_type = db.Column(db.String(10), nullable=False) # Used for differeniating types of user - "student", "staff", "admin"
 
-    def __init__(self, username, password):
+    __mapper_args__ = {
+        'polymorphic_on': user_type,
+        'polymorphic_identity': 'user'
+    }
+
+    def __init__(self, username, password, email, user_type="student"):
         self.username = username
         self.set_password(password)
+        self.email = email
+        self.user_type = user_type
 
     def get_json(self):
         return{
             'id': self.id,
-            'username': self.username
+            'username': self.username,
+            'user_type': self.user_type
         }
 
     def set_password(self, password):
@@ -24,3 +34,39 @@ class User(db.Model):
         """Check hashed password."""
         return check_password_hash(self.password, password)
 
+class Student(User):
+    __tablename__="student"
+    id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
+    hours = db.Column(db.Integer, default=0)
+
+    __mapper_args__={
+        'polymorphic_identity': 'student',
+    }
+
+    def get_accolades(self):
+        milestones = [10, 25, 50]
+        return [m for m in milestones if self.hours >= m]
+    
+class Staff(User):
+    __tablename__="staff"
+    id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
+
+    __mapper_args__={
+        'polymorphic_identity': 'staff',
+    }
+
+class HourLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey("student.id"), nullable=False)
+    staff_id = db.Column(db.Integer, db.ForeignKey("staff.id"), nullable=True)
+    hours = db.Column(db.Integer, nullable=False)
+    confirmed = db.Column(db.Boolean, default=False)
+
+    def get_json(self):
+        return {
+            'id' : self.id,
+            'student_id': self.student_id,
+            'staff_id' : self.staff_id,
+            'hours':self.hours,
+            'confirmed' : self.confirmed
+        }
